4e5493b697530fd6283a9f934065d495ad695b56
robinNcode <msmrobin518@gmail.com>
2021-10-17 03:01:57 +0600

    login done

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -8,11 +8,13 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation _animation;
   late TextEditingController _emailController = TextEditingController(text: '');
-  late TextEditingController _passwordController = TextEditingController(text: '');
+  late TextEditingController _passwordController =
+      TextEditingController(text: '');
   bool _obscureText = true;
 
   @override
@@ -23,8 +25,10 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
 
   @override
   void initState() {
-    _animationController = AnimationController(vsync: this, duration: Duration(seconds: 20));
-    _animation = CurvedAnimation(parent: _animationController, curve: Curves.easeIn)
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 20));
+    _animation =
+        CurvedAnimation(parent: _animationController, curve: Curves.linear)
           ..addListener(() {
             setState(() {});
           })
@@ -46,7 +50,8 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
       body: Stack(
         children: [
           CachedNetworkImage(
-            imageUrl: "http://robin.adovasoft.com/My_portfolio/public/img/elements/g8.jpeg",
+            imageUrl:
+                "http://robin.adovasoft.com/My_portfolio/public/img/elements/g21.jpeg",
             placeholder: (context, url) => Image.asset(
               'assets/images/login1.jpg',
               fit: BoxFit.fill,
@@ -69,31 +74,33 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
             padding: EdgeInsets.symmetric(horizontal: _size.width / 30),
             child: ListView(
               children: [
-                SizedBox(height: _size.height * 0.1,),
-                const Text('Login', style: TextStyle(fontSize: 30, color: Colors.white),),
+                SizedBox(
+                  height: _size.height * 0.1,
+                ),
+                const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
                 SizedBox(height: 10),
                 RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'Don\'t have account?  ',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-                      TextSpan(
-                        text: ' Register',
-                        style: TextStyle(fontSize: 18, color: Colors.blue),
-                      ),
-                    ]
-                  ),
+                  text: TextSpan(children: [
+                    TextSpan(
+                      text: 'Don\'t have account?  ',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    TextSpan(
+                      text: ' Register',
+                      style: TextStyle(fontSize: 18, color: Colors.blue),
+                    ),
+                  ]),
                 ),
                 SizedBox(height: 40),
                 TextFormField(
                   controller: _emailController,
-                  validator: (value){
-                    if(value!.isEmpty || !value.contains('@')){
+                  validator: (value) {
+                    if (value!.isEmpty || !value.contains('@')) {
                       return 'Please Enter Valid Email';
-                    }
-                    else{
+                    } else {
                       return null;
                     }
                   },
@@ -104,39 +111,44 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
                     labelStyle: TextStyle(color: Colors.white),
                     hintStyle: TextStyle(color: Colors.white),
                     hintText: 'm.....@..com',
-                    enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                    focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.yellow)),
-
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white)),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.yellow)),
                   ),
                 ),
-
                 SizedBox(height: 10),
                 TextFormField(
                   controller: _passwordController,
-                  validator: (value){
-                    if(value!.isEmpty || value.length < 7){
+                  validator: (value) {
+                    if (value!.isEmpty || value.length < 7) {
                       return 'Please don\'t enter getter than 6';
-                    }
-                    else{
+                    } else {
                       return null;
                     }
                   },
                   keyboardType: TextInputType.emailAddress,
                   style: TextStyle(color: Colors.white),
                   decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: Colors.white),
-                    hintStyle: TextStyle(color: Colors.white),
-                    hintText: '......',
-                    enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                    focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.yellow)),
-                    suffixIcon: GestureDetector(
-                      onTap: (){
-                        _obscureText = !_obscureText;
-                      },
-                      child: Icon(_obscureText ? Icons.visibility_off : Icons.visibility, color: Colors.white,),
-                    )
-                  ),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.white),
+                      hintStyle: TextStyle(color: Colors.white),
+                      hintText: 'password',
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.yellow)),
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          _obscureText = !_obscureText;
+                        },
+                        child: Icon(
+                          _obscureText
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                          color: Colors.white,
+                        ),
+                      )),
                   obscureText: _obscureText,
                 ),
               ],
