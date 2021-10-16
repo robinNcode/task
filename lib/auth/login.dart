44723763816bf7b466de7ec128a3abd665543b11
robinNcode <msmrobin518@gmail.com>
2021-10-16 18:40:30 +0600

    login page done

---
+++ lib/auth/login.dart
@@ -0,0 +1,149 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation _animation;
+  late TextEditingController _emailController = TextEditingController(text: '');
+  late TextEditingController _passwordController = TextEditingController(text: '');
+  bool _obscureText = true;
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _animationController = AnimationController(vsync: this, duration: Duration(seconds: 20));
+    _animation = CurvedAnimation(parent: _animationController, curve: Curves.easeIn)
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((animationStatus) {
+            if (animationStatus == AnimationStatus.completed) {
+              _animationController.reset();
+              _animationController.forward();
+            }
+          });
+    _animationController.forward();
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    Size _size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          CachedNetworkImage(
+            imageUrl: "http://robin.adovasoft.com/My_portfolio/public/img/elements/g8.jpeg",
+            placeholder: (context, url) => Image.asset(
+              'assets/images/login1.jpg',
+              fit: BoxFit.fill,
+            ),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+            alignment: FractionalOffset(_animation.value, 0),
+          ),
+          //For asset image animation
+          // Image.asset(
+          //   'assets/images/login.jpeg',
+          //   width: double.infinity,
+          //   height: double.infinity,
+          //   fit: BoxFit.cover,
+          //   alignment: FractionalOffset(_animation.value, 0),
+          // ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: _size.width / 30),
+            child: ListView(
+              children: [
+                SizedBox(height: _size.height * 0.1,),
+                const Text('Login', style: TextStyle(fontSize: 30, color: Colors.white),),
+                SizedBox(height: 10),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Don\'t have account?  ',
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      TextSpan(
+                        text: ' Register',
+                        style: TextStyle(fontSize: 18, color: Colors.blue),
+                      ),
+                    ]
+                  ),
+                ),
+                SizedBox(height: 40),
+                TextFormField(
+                  controller: _emailController,
+                  validator: (value){
+                    if(value!.isEmpty || !value.contains('@')){
+                      return 'Please Enter Valid Email';
+                    }
+                    else{
+                      return null;
+                    }
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.white),
+                    hintStyle: TextStyle(color: Colors.white),
+                    hintText: 'm.....@..com',
+                    enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                    focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.yellow)),
+
+                  ),
+                ),
+
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: _passwordController,
+                  validator: (value){
+                    if(value!.isEmpty || value.length < 7){
+                      return 'Please don\'t enter getter than 6';
+                    }
+                    else{
+                      return null;
+                    }
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.white),
+                    hintStyle: TextStyle(color: Colors.white),
+                    hintText: '......',
+                    enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                    focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.yellow)),
+                    suffixIcon: GestureDetector(
+                      onTap: (){
+                        _obscureText = !_obscureText;
+                      },
+                      child: Icon(_obscureText ? Icons.visibility_off : Icons.visibility, color: Colors.white,),
+                    )
+                  ),
+                  obscureText: _obscureText,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'auth/login.dart';
+
+void main(){
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
